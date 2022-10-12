ecdcf74670df58112b44ad356afceee67123aa11
Serhat TURKER <48413508+turkerdev@users.noreply.github.com>
2022-10-12 11:01:19 +0300

    Add AdSense script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,23 +4,33 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import NavBar from "../components/NavBar";
+import Head from "next/head";
 
 const queryClient = new QueryClient();
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NavBar />
-      <Component {...pageProps} />
-      <ToastContainer
-        bodyClassName="font-jetbrains tracking-tighter text-amber-400 text-sm"
-        toastClassName="border border-neutral-700"
-        position="bottom-right"
-        autoClose={3000}
-        theme="dark"
-        hideProgressBar={true}
-      />
-    </QueryClientProvider>
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4278833630932343"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <NavBar />
+        <Component {...pageProps} />
+        <ToastContainer
+          bodyClassName="font-jetbrains tracking-tighter text-amber-400 text-sm"
+          toastClassName="border border-neutral-700"
+          position="bottom-right"
+          autoClose={3000}
+          theme="dark"
+          hideProgressBar={true}
+        />
+      </QueryClientProvider>
+    </>
   );
 }
 
